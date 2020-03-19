0b1dc892c39a36572036977891008118db0285e1
Abby Blachman <abbyblachman@Abbys-MacBook-Pro.local>
2020-03-19 18:30:58 -0500

    adding responsive design

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,19 +1,36 @@
 import React, {useState, useEffect} from 'react';
 import Component from '../components/Component';
 import Leaders from '../components/Leaders'
+import MediaQuery from 'react-responsive'
+ 
 
 function Home () {
+
+    
 
 const style = {
     paddingBottom: '5rem', 
     margin: '5rem'
 }
 
+const styleSmall = {
+    display: 'flex', 
+    flexDirection: 'column', 
+    fontSize: '1.5rem'
+}
+
     return(
-        <div className="container" style={style}>
-            
+    <div className="container" style={style}>
+   
+   <MediaQuery minDeviceWidth={1224} device={{ deviceWidth: 1600 }}>
     <Component styles={style}></Component>
     <Leaders styles={style}></Leaders>
+    </MediaQuery>
+    <MediaQuery maxDeviceWidth={1224} device={{ deviceWidth: 1600 }}>
+    <Component styles={styleSmall}></Component>
+    <Leaders styles={styleSmall}></Leaders>
+    </MediaQuery>
+
     </div>
     )
 }
