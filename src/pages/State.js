df0a9b445b081792348d4ced624c8dfcc7487acc
abbyblachman <abbysblachman@gmail.com>
2020-03-22 18:18:42 -0500

    changing font

--- src/pages/State.js
+++ src/pages/State.js
@@ -140,7 +140,8 @@ function onClick(event) {
         marginTop: '1rem'
     }
     const formSpanLarge = {
-        fontSize: '1.5rem'
+        fontSize: '1.5rem', 
+        fontFamily: 'Bree Serif'
     }
 
 
