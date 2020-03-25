8be5c21b43953a9aa758b134fb17931234df036f
Hannah Chamorro <hannah.chamorro@gmail.com>
2020-03-25 14:03:27 -0500

    init NY data fetch and set context on home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,16 +5,19 @@ import Map from '../components/MapHC';
 import MediaQuery from 'react-responsive';
 import { AllStatesDataContext } from '../utils/AllStatesDataContext';
 import { USADataContext } from '../utils/USAData';
+import { StateDataContext } from '../utils/StateData';
 import Axios from 'axios';
 import moment from 'moment';
 
 function Home() {
   const [AllStatesData, setAllStatesData] = useContext(AllStatesDataContext);
   const [USAData, setUSAData] = useContext(USADataContext);
+  const [stateData, setStateData] = useContext(StateDataContext);
 
   useEffect(() => {
     fetchAllStatesData();
     fetchUSAData();
+    fetchStateDataInit();
   }, []);
 
   function fetchAllStatesData() {
@@ -27,6 +30,12 @@ function Home() {
     Axios.get('https://covidtracking.com/api/us').then(res =>
       setUSAData(res.data)
     );
+  }
+
+  function fetchStateDataInit() {
+    Axios.get(
+      'https://cors-anywhere.herokuapp.com/https://covidtracking.com/api/states/daily?state=NY'
+    ).then(res => setStateData(res.data));
   }
 
   const style = {
@@ -52,7 +61,7 @@ function Home() {
 
   return (
     <>
-      {console.log('this shoule an array with an object of 6 vars', USAData)}
+      {console.log('this shoule be new york data', stateData)}
       <div className="container" style={style}>
         <MediaQuery maxDeviceWidth={1223} device={{ deviceWidth: 1599 }}>
           <Component styles={styleSmall}></Component>
