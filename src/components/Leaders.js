c0630244cb4b74110cbf1ecb2335063ab02c7234
abbyblachman <abbysblachman@gmail.com>
2020-03-27 08:53:18 -0500

    fixing daily leaders bug

--- src/components/Leaders.js
+++ src/components/Leaders.js
@@ -39,11 +39,14 @@ function getData() {
 axios
 .get(`https://cors-anywhere.herokuapp.com/https://covidtracking.com/api/states/daily?date=${now}`)
 .then(res => {
-    console.log('in the then')
+    // console.log('in the then')
     if (res.data.error === true) {
+        // console.log(res.data)
         axios
             .get(`https://cors-anywhere.herokuapp.com/https://covidtracking.com/api/states/daily?date=${startdate}`)
             .then(res => {
+                // console.log('in the if')
+                console.log(res.data.length)
                 res.data.forEach(data => {
                     arr.push({
                         state: data.state,
@@ -51,29 +54,39 @@ axios
                         total: data.total})
                 })
                 arr.sort(function(a, b){ return b.positive - a.positive});
-                setPositive(arr);
-            })
-    } else {
-        res.data.forEach(data => {
-                arr.push({
-                    state: data.state,
-                    positive: data.positive, 
-                    total: data.total})
-    })
-    arr.sort(function(a, b){ return b.positive - a.positive});
-    setPositive(arr);
-    console.log(arr)
-    }   
-})
-            .then(() => {
                 setStateOne(arr[0])
                 setStateTwo(arr[1])
                 setStateThree(arr[2])
                 setStateFour(arr[3])
                 setStateFive(arr[4])
+                // console.log(arr)
             })
-           
+    } else if (res.data.length > 50) {
+                res.data.forEach(data => {
+                        arr.push({
+                            state: data.state,
+                            positive: data.positive, 
+                            total: data.total})
+            })
+            arr.sort(function(a, b){ return b.positive - a.positive});
+            setStateOne(arr[0])
+            setStateTwo(arr[1])
+            setStateThree(arr[2])
+            setStateFour(arr[3])
+            setStateFive(arr[4])
         }
+        })        // console.log(arr)
+    }   
+
+            // .then(() => {
+            //     setStateOne(arr[0])
+            //     setStateTwo(arr[1])
+            //     setStateThree(arr[2])
+            //     setStateFour(arr[3])
+            //     setStateFive(arr[4])
+            // })
+           
+
       
     
 
@@ -103,15 +116,19 @@ const subHeadSmall = {
     fontSize: '0.5rem'
 }
 
+// console.log(stateOne)
+// console.log(stateTwo)
+
 return(
     <div style={style}>
         {console.log(stateOne)}
+        {console.log(stateOne)}
         <div style={span}>States with the most positive tests so far:</div>
-        <div><Link to={`/${stateOne.state}`}><strong style={a}>{stateOne.state}</strong></Link>: {stateOne.positive} cases statewide. {Math.floor((stateOne.positive/usaPostive)*100)}% of cases nationwide are in this state.</div>
+         <div><Link to={`/${stateOne.state}`}><strong style={a}>{stateOne.state}</strong></Link>: {stateOne.positive} cases statewide. {Math.floor((stateOne.positive/usaPostive)*100)}% of cases nationwide are in this state.</div>
         <div><Link to={`/${stateTwo.state}`}><strong style={a}>{stateTwo.state}</strong></Link>: {stateTwo.positive} cases statewide. {Math.floor((stateTwo.positive/usaPostive)*100)}% of cases nationwide are in this state.</div>
         <div><Link to={`/${stateThree.state}`}><strong style={a}>{stateThree.state}</strong></Link>: {stateThree.positive} cases statewide. {Math.floor((stateThree.positive/usaPostive)*100)}% of cases nationwide are in this state.</div>
         <div><Link to={`/${stateFour.state}`}><strong style={a}>{stateFour.state}</strong></Link>: {stateFour.positive} cases statewide. {Math.floor((stateFour.positive/usaPostive)*100)}% of cases nationwide are in this state.</div>
-        <div><Link to={`/${stateFive.state}`}><strong style={a}>{stateFive.state}</strong></Link>: {stateFive.positive} cases statewide. {Math.floor((stateFive.positive/usaPostive)*100)}% of cases nationwide are in this state.</div>
+         <div><Link to={`/${stateFive.state}`}><strong style={a}>{stateFive.state}</strong></Link>: {stateFive.positive} cases statewide. {Math.floor((stateFive.positive/usaPostive)*100)}% of cases nationwide are in this state.</div>
         <div style={subHeadSmall}>Built w/ ❤️ by Abby Blachman and Hannah Chamorro. View the <a href="https://github.com/abbyblachman/covid-dashboard">source code</a> on GitHub.</div>
     </div>
     )
